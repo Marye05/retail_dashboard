926470e437aba04bfba2fa0759e278c37b2a4cfe
Maryeris Orozco <maryeris527@gmail.com>
2025-08-03 22:29:11 -0400

    add pagination component

---
+++ src/components/ProductPagination.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center mt-4 gap-2">
+    <button
+      v-for="page in totalPages"
+      :key="page"
+      :class="['px-3 py-1 rounded border', { 'bg-blue-500 text-white': currentPage === page }]"
+      @click="$emit('change', page)"
+    >
+      {{ page }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+defineProps(['currentPage', 'totalPages']);
+defineEmits(['change']);
+</script>
